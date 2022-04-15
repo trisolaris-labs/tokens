eb7004e5b8213a60a6ef9584eda7512532d35c3e
memacius32 <memacius32@protonmail.com>
2022-04-15 10:58:24 -0300

    Add ethlogo

--- format.mjs
+++ format.mjs
@@ -1,11 +1,14 @@
-import {join} from 'node:path';
-import {renameSync, readdirSync} from 'node:fs';
-import {getAddress} from '@ethersproject/address';
+import { join } from 'node:path';
+import { renameSync, readdirSync } from 'node:fs';
+import { getAddress } from '@ethersproject/address';
 
 const ASSETS = './assets';
+const excludedFiles = ["ethlogo.svg"]
 
-const dirs = readdirSync(ASSETS).filter(name => name !== getAddress(name));
+const dirs = readdirSync(ASSETS)
+  .filter((name) => !excludedFiles.includes(name))
+  .filter((name) => name !== getAddress(name));
 
 for (const name of dirs) {
-	renameSync(join(ASSETS, name), join(ASSETS, getAddress(name)));
+  renameSync(join(ASSETS, name), join(ASSETS, getAddress(name)));
 }
